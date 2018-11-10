8ef21395998870b706648d6131a90d208d08fcff
dmwo <37816602+dmwo@users.noreply.github.com>
2018-11-10 14:17:53 -0700

    simplifying the test
    Simple Reverb

--- test1/test2.ino
+++ test1/test2.ino
@@ -5,28 +5,11 @@
 #include <SerialFlash.h>
 
 // GUItool: begin automatically generated code
-AudioInputI2S            lineIn;           //xy=62,32
-AudioEffectBitcrusher    bitcrusher1;    //xy=224,327
-AudioEffectReverb        reverb1;        //xy=235,27
-AudioEffectMultiply      multiply1;      //xy=348,207
-AudioEffectMultiply      multiply2;      //xy=372,328
-AudioSynthNoiseWhite     noise1;         //xy=377,25
-AudioEffectEnvelope      envelope1;      //xy=518,31
-AudioMixer4              mixer1;         //xy=563,180
-AudioEffectChorus        chorus1;        //xy=763,112
-AudioOutputI2S           lineOut;           //xy=777,515
-AudioConnection          patchCord1(lineIn, 0, reverb1, 0);
-AudioConnection          patchCord2(lineIn, 1, bitcrusher1, 0);
-AudioConnection          patchCord3(lineIn, 1, multiply1, 1);
-AudioConnection          patchCord4(bitcrusher1, 0, multiply2, 0);
-AudioConnection          patchCord5(bitcrusher1, 0, multiply2, 1);
-AudioConnection          patchCord6(reverb1, 0, multiply1, 0);
-AudioConnection          patchCord7(multiply1, 0, mixer1, 1);
-AudioConnection          patchCord8(multiply2, 0, mixer1, 2);
-AudioConnection          patchCord9(noise1, envelope1);
-AudioConnection          patchCord10(envelope1, 0, mixer1, 0);
-AudioConnection          patchCord11(mixer1, chorus1);
-AudioConnection          patchCord12(chorus1, 0, lineOut, 0);
-AudioConnection          patchCord13(chorus1, 0, lineOut, 1);
-AudioControlSGTL5000     sgtl5000_1;     //xy=67.5,86
+AudioInputI2S            lineIn;         //xy=66,43
+AudioEffectChorus        chorus1;        //xy=446,45
+AudioOutputI2S           lineOut;        //xy=653,52
+AudioConnection          patchCord1(lineIn, 0, chorus1, 0);
+AudioConnection          patchCord2(chorus1, 0, lineOut, 0);
+AudioConnection          patchCord3(chorus1, 0, lineOut, 1);
+AudioControlSGTL5000     sgtl5000_1;     //xy=71,97
 // GUItool: end automatically generated code
